fd47a697f09e0636805efe44324b86a1cb106e64
Kafrawi <hassaballaelkafrawi@gmail.com>
2024-11-15 17:25:25 +0100

    Refactor: Ensuring processesed Dataset focuses on the correct Spain region
    - added x and y parameters for prepare_climate_dataset under DataTransformer to help focus on required spatial region
    - Ensured processes dataframes are saved under climate in a dedicated forlder

--- preliminary_data_prepartion/prepare_climate_dataset.py
+++ preliminary_data_prepartion/prepare_climate_dataset.py
@@ -5,10 +5,13 @@ import os
 folder_path = "raw_data\climate"
 climate_datasets = os.listdir(folder_path)
 
-zip_files = [file for file in climate_datasets if file.endswith(".zip")]
-for file in zip_files:
-    file_path = os.path.join(folder_path,file)
-    eub.unzip_files(file_path,folder_path)
+try:
+    zip_files = [file for file in climate_datasets if file.endswith(".zip")]
+    for file in zip_files:
+        file_path = os.path.join(folder_path,file)
+        eub.unzip_files(file_path,folder_path)
+except Exception as e:
+    print(e)
 
 nc_files = [file for file in climate_datasets if file.endswith(".nc")]
 
@@ -16,7 +19,7 @@ for file in nc_files:
     print(f"Processing {file}")
     file_path = os.path.join(folder_path,file)
     df = dt.prepare_climate_dataset(file_path)
-    eub.save_dataframe(df,f"PreliminaryProcess_{file}",location=("process_data\\pickle",
-                                                          "process_data\\csv"))
+    eub.save_dataframe(df,f"PreliminaryProcess_{file}",location=("process_data\\pickle\\climate",
+                                                          "process_data\\csv\\climate"))
 
 
