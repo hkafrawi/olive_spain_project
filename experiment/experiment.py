e423aa787efaf7bbf7fbb0431b5005110e0a088b
Kafrawi <hassaballaelkafrawi@gmail.com>
2024-11-21 22:53:56 +0100

    Add: initalize experiment class

---
+++ experiment/experiment.py
@@ -0,0 +1,24 @@
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV 
+
+class Experiment:
+    def __init__(self):
+        pass
+
+    def train_test_split(self):
+        pass
+
+    def date_split(self):
+        pass
+
+    def setup_pipeline(self):
+        pass
+
+    def run_search(self):
+        pass
+
+    def run_pipeline(self):
+        pass
+
+    def evaluate(self):
+        pass
+    
