087f2eb3639de027b0a0fa2cc76bdb1d379a06da
Kafrawi <hassaballaelkafrawi@gmail.com>
2024-12-06 20:55:53 +0100

    Refactor: Updating start.sh
    - Starting without git lfs

--- start.sh
+++ start.sh
@@ -9,14 +9,6 @@ if ! command -v git &> /dev/null; then
 fi
 echo "Git is installed."
 
-# Install Git LFS (without root access)
-echo "Installing Git LFS..."
-git lfs install
-
-# Pull Git LFS data (this assumes LFS pointers are already in the repo)
-echo "Pulling Git LFS data..."
-git lfs pull
-
 # Install Python dependencies
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
